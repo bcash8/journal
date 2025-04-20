94cf6240c27685d4f9bffc8992ece55de26d9644
Ben Cash <benjamincash88@gmail.com>
2025-04-19 21:03:16 -0600

    start notes functionality

--- web/src/components/NoteTags/NoteTags.tsx
+++ web/src/components/NoteTags/NoteTags.tsx
@@ -1,3 +1,25 @@
+import { Plus, Tag } from "@phosphor-icons/react";
+import { useNote } from "../../hooks/useNote";
+import styles from "./NoteTags.module.css";
+
 export function NoteTags() {
-  return <div></div>;
+  const note = useNote();
+  return (
+    <div className={styles.noteContainer}>
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+        <h3 style={{ display: "flex", alignItems: "center" }}>
+          <Tag size={28} />
+        </h3>
+        <button>
+          New
+          <Plus weight="bold" />
+        </button>
+      </div>
+      <ul>
+        {note.tags?.map((tag) => (
+          <li key={tag.id}>{tag.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
