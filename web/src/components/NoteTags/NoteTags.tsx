93ed5c16c4d0939fc37a2612d4684d922e9a55a2
Ben Cash <benjamincash88@gmail.com>
2025-04-20 21:01:04 -0600

    Start work on new note modal

--- web/src/components/NoteTags/NoteTags.tsx
+++ web/src/components/NoteTags/NoteTags.tsx
@@ -1,25 +1,68 @@
 import { Plus, Tag } from "@phosphor-icons/react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import { useNote } from "../../hooks/useNote";
 import styles from "./NoteTags.module.css";
 
 export function NoteTags() {
   const note = useNote();
+  const [modalOpen, setModalOpen] = useState(true);
+
   return (
-    <div className={styles.noteContainer}>
-      <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-        <h3 style={{ display: "flex", alignItems: "center" }}>
-          <Tag size={28} />
-        </h3>
-        <button>
-          New
-          <Plus weight="bold" />
-        </button>
+    <>
+      <div className={styles.noteContainer}>
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+          <h3 style={{ display: "flex", alignItems: "center" }}>
+            <Tag size={28} />
+          </h3>
+          <button onClick={() => setModalOpen(true)}>
+            New
+            <Plus weight="bold" />
+          </button>
+        </div>
+        <ul>
+          {note.tags?.map((tag) => (
+            <li key={tag.id}>{tag.name}</li>
+          ))}
+        </ul>
       </div>
-      <ul>
-        {note.tags?.map((tag) => (
-          <li key={tag.id}>{tag.name}</li>
-        ))}
-      </ul>
-    </div>
+      {modalOpen && (
+        <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+          <p>content</p>
+        </Modal>
+      )}
+    </>
+  );
+}
+
+function Modal({ isOpen, onClose, children }: { isOpen: boolean; onClose?: () => void; children?: ReactNode }) {
+  const dialogRef = useRef<HTMLDialogElement | null>(null);
+  const contentRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const dialog = dialogRef.current;
+    if (!dialog) return;
+
+    if (isOpen) {
+      dialog.showModal();
+    } else {
+      dialog.close();
+    }
+
+    function handleClickOutside(e: MouseEvent) {
+      if (contentRef.current && e.target instanceof Node && !contentRef.current.contains(e.target)) onClose?.();
+    }
+
+    dialog.addEventListener("click", handleClickOutside);
+    return () => {
+      dialog.removeEventListener("click", handleClickOutside);
+    };
+  }, [isOpen, onClose]);
+
+  return (
+    <dialog ref={dialogRef} onCancel={onClose} className={styles.modal}>
+      <div ref={contentRef} className={`${styles.content}`}>
+        {children}
+      </div>
+    </dialog>
   );
 }
